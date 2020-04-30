5b82a93dca61c91f63453f13778d7240cb296ea9
Addy Hebou <addyhebou@Addys-MacBook-Pro.local>
2020-04-29 21:06:57 -0400

    its been a while

--- nightMode.js
+++ nightMode.js
@@ -10,8 +10,6 @@ function turnOnNight(){
     document.querySelectorAll('p').forEach(e => e.style.color = "white");
     document.getElementById('grayBox').style.backgroundColor = "#262626";
     document.getElementById('bio').style.color = "white";
-    // document.getElementById('day').style.display = "none";
-    // document.getElementById('night').style.display = "block";
     document.querySelectorAll('header').forEach(e => e.style.borderTop = "6px solid rgb(224, 127, 224)");
     document.querySelectorAll('header').forEach(e => e.style.backgroundColor = "#262626");
 }
@@ -24,8 +22,6 @@ function turnOffNight(){
     document.querySelectorAll('i').forEach(e => e.style.color = "black");
     document.querySelectorAll('p').forEach(e => e.style.color = "black");
     document.getElementById('grayBox').style.backgroundColor = "white";
-    // document.getElementById('day').style.display = "block";
-    // document.getElementById('night').style.display = "none";
     document.querySelectorAll('header').forEach(e => e.style.borderTop = "6px solid rgb(255,255,255)");
     document.querySelectorAll('header').forEach(e => e.style.backgroundColor = "white");
 
