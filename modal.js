a468635e1c9206b55c86f08a6194f06a0dad0480
Addy Hebou <addyhebou@Addys-MacBook-Pro.local>
2020-11-06 23:53:10 -0500

    feat(modal): modal works, just slow to close

--- modal.js
+++ modal.js
@@ -1,6 +1,13 @@
 console.log("🍆🍆🍆🍆🍆🍆🍆🍆🍆🍆")
 function showdiv(){
     document.getElementById("openModal").style.visibility="visible";
-    console.log("🔥🔥🔥🔥🔥🔥🔥")
+    console.log("SHOWN DIV🔥🔥🔥🔥🔥🔥🔥")
+}
+function closediv(){
+    document.getElementById("openModal").style.visibility="hidden";
+    document.getElementById("if").style.visibility="hidden";
+    // document.getElementById("modal-content").style.visibility="hidden";
+    console.log("CLOSED all🔥🔥🔥🔥🔥🔥🔥")
 }
 setTimeout("showdiv()", 1500);
+
